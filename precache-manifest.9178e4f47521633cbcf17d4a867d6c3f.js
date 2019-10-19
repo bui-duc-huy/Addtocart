540ae14df1920680344cc6d6473bd523ba826846
bui-duc-huy <53326016+bui-duc-huy@users.noreply.github.com>
2019-10-20 01:12:46 +0700

    Add files via upload

---
+++ precache-manifest.9178e4f47521633cbcf17d4a867d6c3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4026a3e9b53a31cc2e62926d895e88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38da3ec5b6f1c14a7b59",
+    "url": "/static/css/main.7ef09a99.chunk.css"
+  },
+  {
+    "revision": "89e1ada20d33bbd64f1c",
+    "url": "/static/js/2.d9899807.chunk.js"
+  },
+  {
+    "revision": "38da3ec5b6f1c14a7b59",
+    "url": "/static/js/main.0d8726c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
